57628e791a0033e249a6a95bc350d222d5250e38
Azkali <a.ffcc7@gmail.com>
2022-11-22 23:55:59 +0100

    Add docker as argument for local build

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -xe
+if [[ $1 = "docker" ]]; then
+	docker build -t fold .
+	docker run --rm -it -v "$PWD":/build fold
+fi
+
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
 ./build/build.sh "$@"
