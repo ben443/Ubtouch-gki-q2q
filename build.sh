2626007a93acc9b7c7705a6b047e69ccd1d63343
Azkali <a.ffcc7@gmail.com>
2022-11-25 14:06:54 +0100

    Add privileged arg to docker run needed to use devices

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -xe
 if [[ $1 = "docker" ]]; then
 	docker build -t fold .
-	docker run --rm -it -v "$PWD":/build fold
+	docker run --privileged --rm -it -v "$PWD":/build fold
 fi
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
