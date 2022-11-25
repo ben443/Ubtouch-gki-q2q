f2c766cd485feb040e1c4de6d6a82097ae649617
Azkali <a.ffcc7@gmail.com>
2022-11-25 15:42:07 +0100

    Dockerfile: exit after build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -xe
 if [[ $1 = "docker" ]]; then
 	docker build -t fold .
 	docker run --privileged --rm -it -v "$PWD":/build fold
+	exit
 fi
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
