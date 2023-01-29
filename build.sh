202234ac8bd276c2931322c4fe7b334bb3d9e171
Azkali Manad <a.ffcc7@gmail.com>
2023-01-29 15:16:01 +0100

    remove hack enabling ssh

--- build.sh
+++ build.sh
@@ -7,18 +7,7 @@ if [[ $1 = "docker" ]]; then
 fi
 
 [ -d build ] || git clone https://gitlab.com/Azkali/halium-generic-adaptation-build-tools -b halium-11-focal build
-./build/build.sh "$@"
+./build/build.sh -b . "$@"
 ./build/prepare-fake-ota.sh ./out/device_q2q_usrmerge.tar.xz ota
 ./build/system-image-from-ota.sh ota/ubuntu_command out
 mv out/rootfs.img out/ubuntu.img
-
-mount out/ubuntu.img
-
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-DEBIAN_FRONTEND=noninteractive \
-LANG=C RUNLEVEL=1 \
-chroot /mnt /bin/bash <<EOF
-echo phablet:phablet | chpasswd
-EOF
-
-umount out/ubuntu.img
