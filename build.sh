692774ee464e2dc5d85e68840358ad60697c116f
ben443 <64079735+ben443@users.noreply.github.com>
2024-01-12 02:25:12 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ fi
 
 [ -d build ] || git clone https://github.com/ben443/halium-generic-adaptation-build-tools -b halium-12 build
 ./build/build.sh "$@"
-./build/prepare-fake-ota.sh ./out/device_q2q_usrmerge.tar.xz ota
+./build/prepare-fake-ota.sh ./out/device_r8q_usrmerge.tar.xz ota
 ./build/system-image-from-ota.sh ota/ubuntu_command out
 mv out/rootfs.img out/ubuntu.img
